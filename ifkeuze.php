645774971431f989a5335b3709fe52a6f5e39e0a
edwin <97094582@st.deltion.nl>
2022-03-16 12:12:10 +0100

    ifkeuze10.1

---
+++ ifkeuze.php
@@ -0,0 +1,20 @@
+<?php
+
+    $naam = readline("Geef jouw naam : ");
+    $leeftijd = readline("Geef jouw leeftijd : ");
+
+        echo "Hallo $naam \n";
+
+    if ( $leeftijd < 22 ) {
+        echo "Jij bent jonger dan 22 jaar.\n";
+    }
+
+    if ( $leeftijd > 22 ) {
+        echo "Jij bent ouder dan 22 jaar.\n";
+    }
+
+    if ( $leeftijd == 22 ) {
+        echo "Jij bent precies 22 jaar.\n";
+    }
+   
+?>
