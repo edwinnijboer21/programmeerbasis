66bd83dd6d7645eb2bd592eaa3034bb2e6488013
edwin <97094582@st.deltion.nl>
2022-03-16 09:46:22 +0100

    forlus9.1

--- test.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $leeftijd = 21;
-    echo "Ik ben $leeftijd jaar oud <br>";
-    echo "Ik woon in westerhaar <br>";
-    echo "Ik wil graag ooit naar de stad verhuizen";
-    echo "Ik ben nu $leeftijd maar over een paar maand word ik 22 <br>";
-
-
-?>
