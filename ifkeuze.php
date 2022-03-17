e28507e9cbe9a8251b0fbd4998fa2b942066db08
edwin <97094582@st.deltion.nl>
2022-03-17 09:40:58 +0100

    ifkeuze10.2

--- ifkeuze.php
+++ ifkeuze.php
@@ -5,16 +5,16 @@
 
         echo "Hallo $naam \n";
 
-    if ( $leeftijd < 22 ) {
-        echo "Jij bent jonger dan 22 jaar.\n";
+    if ( $leeftijd < 25 ) {
+        echo "Jij bent jonger dan 25 jaar.\n";
     }
 
-    if ( $leeftijd > 22 ) {
-        echo "Jij bent ouder dan 22 jaar.\n";
+    if ( $leeftijd > 25 ) {
+        echo "Jij bent ouder dan 25 jaar.\n";
     }
 
-    if ( $leeftijd == 22 ) {
-        echo "Jij bent precies 22 jaar.\n";
+    if ( $leeftijd == 25 ) {
+        echo "Jij bent precies 25 jaar.\n";
     }
    
 ?>
